6219a06fdc7863150602447623f03c763812499b
Tragidra <astralgg@mail.ru>
2026-04-30 14:17:53 +0300

    add build-time version package

---
+++ pkg/version/version.go
@@ -0,0 +1,7 @@
+// Package version holds build-time version information injected via ldflags.
+package version
+
+// Version is the semantic version string
+var Version = "0.2.0"
+
+var Commit = "unknown"
